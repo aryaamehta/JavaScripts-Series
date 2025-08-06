d4bd10a76255d500886f0a27bc35b5ad8936a841
aryaamehta <153926428+aryaamehta@users.noreply.github.com>
2025-08-06 18:00:02 +0000

    data config

---
+++ Basics/conversionOprn.js
@@ -0,0 +1,8 @@
+let score="33abc"
+let num = 33
+ console.log(typeof score);
+ console.log(typeof(num));
+
+ let valueINNumber = Number(score)
+ console.log(typeof valueINNumber);
+ console.log(valueINNumber);
