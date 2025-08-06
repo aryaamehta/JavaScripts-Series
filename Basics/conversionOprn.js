c2865f595f5ba842be876249b36d9e2ac5749c42
aryaamehta <153926428+aryaamehta@users.noreply.github.com>
2025-08-06 18:28:55 +0000

    conversion

--- Basics/conversionOprn.js
+++ Basics/conversionOprn.js
@@ -6,3 +6,45 @@ let num = 33
  let valueINNumber = Number(score)
  console.log(typeof valueINNumber);
  console.log(valueINNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1, false=> 0
+// "" => false
+// "Rishu" => true
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber= 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+// ******************** Operation **********************
+
+let value = 33
+let negValue = -value
+
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "Namste"
+let str2 = "Bharat"
+
+let str3 = str1+str2;
+console.log(str3);                  //NamsteBharat
+console.log("1" + 2);              // 12
+console.log("1" + 2 + 3);         // 123
+console.log(1 + "2");            // 12
+console.log(1 + 2 + "3");       // 33
+console.log(true);             // true
+console.log(+true);           // 1
+console.log(+"");            // 0
+
